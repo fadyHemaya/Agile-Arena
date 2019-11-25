6b7c438062d37d2c1ed03664806824c8bb8c927b
RamyGabra <RamyGabra5@gmail.com>
2019-11-25 14:16:07 +0200

    pull master

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,5 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/user', require('./user'));
+router.use('/project', require('./project'));
+
 
 module.exports = router;
